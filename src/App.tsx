2f522f6c8ddc96ad1cbe547ce4651d31808cfb90
Taras Kazymir <kazemir.t23.23@gmail.com>
2024-11-30 11:24:07 +0200

    init: start Veggieboost project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className='bg-green flex'>
+
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Усі файли у папці src
+  ],
+  theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      xl: "1280px",
+    },
+    extend: {
+      fontFamily: {
+        "dm-sans": ["DM Sans", "sans-serif"]
+      },
+      colors: {
+        "black": "#141414",
+	      "green": "#a1c487",
+	      "light-green": "#eaf7e5",
+	      "orange": "#fc8a11",
+	      "beige": "#faf3e7",
+	      "gray": "#f3f3f3",
+      },
+      backgroundImage: {
+        checked: "url('@/assets/icons/checkbox.svg')",
+      },
+      boxShadow: {
+        "button-hover":
+          "0 1px 2px 0 rgba(0, 0, 0, 0.3), 0 1px 3px 0 rgba(0, 0, 0, 0.15)",
+      },
+    },
+  },
+  plugins: [],
+};
